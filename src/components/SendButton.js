ddb843a190e987a05ab440c924ab40fbc9923a13
ehsankhakian <ehsankhakian@hotmail.com>
2021-08-15 22:13:47 +0430

    fix: on click phone

--- src/components/SendButton.js
+++ src/components/SendButton.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import styles from './SendButton.module.css'
 
-export default function SendButton({ sendButton, phoneNumber }) {
+export default function SendButton({
+  sendButton,
+  phoneNumber = '989937789718'
+}) {
   return (
     <div className={styles.root}>
       <a
         target='_balnk'
         className={styles.button}
-        href={`https://web.whatsapp.com/send?phone=${phoneNumber}`}
+        href={`https://wa.me/${phoneNumber}`}
       >
         {sendButton}
       </a>
